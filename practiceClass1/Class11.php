cb6f5ef3106f3a653490b91c0088bbadb6cbed94
Kabin <kabinghale1@gmail.com>
2024-03-10 08:43:53 +0545

    on 10/3/24

---
+++ practiceClass1/Class11.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Class 11</title>
+</head>
+<body>
+    
+    <form action="Class11.php" method="post">
+        <label>username : </label>
+        <input type="text" name="username" >
+        <br>
+
+        <label>password: </label>
+        <input type="password" name="password" >
+        <br>
+
+        <input type="submit" name="" id="" value="login" className="">
+        
+    </form>
+</body>
+</html>
+
+<?php 
+    
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+
+?>
